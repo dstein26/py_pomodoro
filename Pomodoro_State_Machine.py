098a22867647e97e2ff42a87b99c18c59467bdfd
Daniel Stein <rckstrongo@gmail.com>
2021-09-26 17:16:16 -0400

    Basic Functions working

--- Pomodoro_State_Machine.py
+++ Pomodoro_State_Machine.py
@@ -6,37 +6,63 @@ Created on Sat Sep 25 22:32:37 2021
 """
 
 from abc import ABC, abstractmethod
+from enum import Enum
 
 class Pomodoro_State_Machine:
-    _state = None
-    _cycleCount = 0  # How many cycles have passed
+    class State(Enum):
+        UNINITIALIZED = -1
+        WORKING = 0
+        BREAK_SHORT = 1
+        BREAK_LONG = 2
     
-    # Time variables
-    _timeWork = 25
-    _timeBreak = 5
-    _timeBreakLong = 20
+    _state = None    # Current state
+    _cycleCount = 0  # How many cycles have passed
+    _timer = None    # Timer instance
     
     def __init__(self, timer):
-        timer.stop()
-        timer.setTimer(self._timeWork)
-        timer.attach("time", self.timeOut)
+        self.attachTimer(timer)
         self.transition_to(State_Working())
         
+    def attachTimer(self, timer):
+        self._timer = timer
+        self.timer.attach("time", self.timeOut)
+    
     def transition_to(self, state):
         self._state = state
         self._state.context = self
-    
+        self._state.setupTimer()
+        
     def timeOut(self, timer):
-        self._state.timeEvent(timer)
+        self._state.timeEvent()
         
     @property
     def state(self):
         return self._state.name
+    
+    @property
+    def timer(self):
+        return self._timer
+    
+    @property
+    def cycles(self):
+        return self._cycleCount
+    
+    def incCycles(self):
+        self._cycleCount += 1
+        return self.cycles
+    
+    def resetCycles(self):
+        self._cycleCount = 0
         
         
 class Pomodoro_State(ABC):
-    _name = ""
+    _state = Pomodoro_State_Machine.State.UNINITIALIZED
     _context = None
+    _time = 0
+    
+    @property
+    def time(self):
+        return self._time
     
     @property
     def name(self):
@@ -49,29 +75,42 @@ class Pomodoro_State(ABC):
     @context.setter
     def context(self, context):
         self._context = context
+        
+    def setupTimer(self):
+        self.context.timer.stop()
+        self.context.timer.setTimer(self.time)
     
     @abstractmethod
-    def timeEvent(self, timer):
+    def timeEvent(self):
         pass
     
 class State_Working(Pomodoro_State):
-    def timeEvent(self, timer):
-        timer.stop()
-        timer.setTimer(self.context._timeBreak)    
-        self._context.transition_to(State_Short_Break())
+    _state = Pomodoro_State_Machine.State.WORKING
+    _time = 25 # *60
+    
+    def timeEvent(self):   
+        if (self._context.cycles < 1):
+            self._context.transition_to(State_Short_Break())
+        else:
+            self._context.transition_to(State_Long_Break())
     
 class State_Short_Break(Pomodoro_State):
-    def timeEvent(self, timer):
-        timer.stop()
-        timer.setTimer(self.context._timeWork)
+    _state = Pomodoro_State_Machine.State.BREAK_SHORT
+    _time = 5 # *60
+    
+    def timeEvent(self):
+        self._context.incCycles()
         self._context.transition_to(State_Working())
     
 class State_Long_Break(Pomodoro_State):
-    def timeEvent(self, timer):
-        timer.stop()
-        timer.setTimer(self.context._timeWork)
+    _state = Pomodoro_State_Machine.State.BREAK_LONG
+    _time = 30 # *60
+    
+    def timeEvent(self):
+        self._context.resetCycles()
         self._context.transition_to(State_Working())
         
+# Basic timer for testing    
 class timerBasic:
     def stop(self):
         pass
--- pomodoro.py
+++ pomodoro.py
@@ -14,58 +14,93 @@ class GUI:
     
     _fontTime=('Bell MT', 30)
     _fontButton=('Centaur', 15)
+    _afterID = None
     
-    def __init__(self):
-        # Initialize Timer
-        self.timer = Timer()
-        self.timer.setTimer(25*60)
+    def __init__(self, timer):
+        self.timer = timer
+        self.timer.attach("time", self.bringToTop)
         
-        # Create window    
+        # Create TK window    
         self.top = tk.Tk()
+        self.top.title("Pomodoro Timer")
         
         # Add widgets to window
         # Frames
+        # TODO: Add light indicators to indicate which cycle we are on
+        # TODO: create constants to manage appearance (fg and bg colors)
         self.frmImage = tk.Frame(bg="red", width = 100)
         self.frmImage.pack(side=tk.LEFT, fill=tk.Y)
         self.frmTimer = tk.Frame(bg="white")
         self.frmTimer.pack(fill=tk.X)
         self.frmButtons = tk.Frame()
         self.frmButtons.pack(fill=tk.X)
+        self.frmState = tk.Frame()
+        self.frmState.pack(fill=tk.X)
         
-        # Timer Label
+        # Labels
+        # Label to display time
         self.strTime = tk.StringVar()
+        self.strTime.set("MM:SS")
         self.lblTime = tk.Label(master=self.frmTimer, 
                                 textvariable=self.strTime,
                                 font=self._fontTime)
         self.lblTime.pack()
         
+        # Label to display current state (Working, short break, long break)
+        self.strState = tk.StringVar()
+        self.strState.set("STATE")
+        self.lblState = tk.Label(master=self.frmState,
+                                 textvariable=self.strState,
+                                 font=self._fontTime)
+        self.lblState.pack()
+        
+        # Buttons
+        # TODO: Buttons look should indicate which buttons can be pressed
+        # Button to start timer
         self.btnStart = tk.Button(master=self.frmButtons, 
                                   command=self.timer.start, text="Start",
                                   font=self._fontButton)
         self.btnStart.pack(side = tk.LEFT)
         
+        # Button to pause timer
         self.btnPause = tk.Button(master=self.frmButtons, 
                                   command=self.timer.pause, text="Pause",
                                   font=self._fontButton)
         self.btnPause.pack(side = tk.LEFT)
+        
+        # Button to stop timer
+        # TODO: Should reset the pomo state
         self.btnStop = tk.Button(master=self.frmButtons, 
                                  command=self.timer.stop, text="Stop",
                                   font=self._fontButton)
         self.btnStop.pack(side = tk.LEFT)
         
+        # Bind the close function to clear the pending after function
+        self.top.protocol("WM_DELETE_WINDOW", self.onClose)
+        
+    # Start the GUI after everything is initialized
     def run(self):
         self.timeEvent()
-
         # Run
         self.top.mainloop()
         
+    # Periodic event to update the time
     def timeEvent(self):
         t = self.timer.checkTime()
         (m, s) = convertToMS(t)
         
         self.strTime.set(f"{m:02d}:{s:02d}")
-        self.top.after(300, self.timeEvent)
+        self._afterID = self.top.after(300, self.timeEvent)
         
+    def onClose(self):
+        # Clear the after buffer to remove the annoying warnings
+        self.top.after_cancel(self._afterID)
+        self.top.quit()
+        self.top.destroy()
+        
+    def bringToTop(self, timer):
+        self.top.attributes('-topmost', 1)
+        self.top.attributes('-topmost', 0)
 
 def convertToMS(time):
     m = (int) (time / 60)
@@ -74,8 +109,9 @@ def convertToMS(time):
         
 
 if __name__ == "__main__":
-    gui = GUI()
-    sm = Pomodoro_State_Machine(gui.timer)
+    tmr = Timer()
+    gui = GUI(tmr)
+    sm = Pomodoro_State_Machine(tmr)
     gui.run()
     
  
